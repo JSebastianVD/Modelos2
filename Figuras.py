55cc0ad805598855727e7f0a0a1d65c77b22ea88
JSebastianVD <54341725+JSebastianVD@users.noreply.github.com>
2019-09-17 20:18:03 -0500

    Add files via upload

--- Figuras.py
+++ Figuras.py
@@ -7,9 +7,9 @@ class Figuras:
     def __init__(self):
         self.__marco = Tk()
         self.__marco.title("Linea")
-        self.__marco.geometry("500x500")
-        self.__canvas = Canvas(self.__marco, width=210, height=210, bg="midnight blue")
-        self.__canvas.place(x=0,y=0)
+        self.__marco.geometry("700x500")
+        self.__canvas = Canvas(self.__marco, width=500, height=400, bg="white")
+        self.__canvas.place(x=0,y=100)
         self.__canvas.pack
 
 
@@ -29,16 +29,23 @@ class Figuras:
             else:
                 r = math.sqrt((int(self.x) - int(self.antx)) ** 2 + (int(self.y) - int(self.anty)) ** 2)
                 self.getCanvas().create_oval(int(self.antx) + r, int(self.anty) + r, int(self.antx) - r,
-                                             int(self.anty) - r, fill="black")
+                                             int(self.anty) - r, fill="midnight blue")
                 self.getCanvas().create_rectangle(int(self.antx), int(self.anty), int(self.x), int(self.y),
                                                   fill="yellow")
                 self.getCanvas().create_polygon(int(self.x), int(self.y), int(self.antx), int(self.anty),
                                                 int(self.x), int(self.anty), fill="red")
                 break
+        self.__r1.set(circulo.perimetro(self))
+        self.__r2.set(circulo.area(self))
+        self.__r3.set(cuadrado.perimetro())
+        self.__r4.set(cuadrado.area())
+        self.__r5.set(triangulo.perimetro(self))
+        self.__r6.set(triangulo.area(self))
 
     def modulo(self):
         r = math.sqrt((int(self.x) - int(self.antx)) ** 2 + (int(self.y) - int(self.anty)) ** 2)
         return r
+
     def getCanvas(self):
         return self.__canvas
 
@@ -52,8 +59,29 @@ class Figuras:
         pass
 
 
-    def mostrar(self):
+    def configurar(self):
         self.__canvas.bind("<Button-1>", self.eventoMouse)
+        # Circulo
+        self.__r1 = StringVar()
+        self.__r2 = StringVar()
+        self.l1 = Label(self.__marco, text = "Perimetro = ").place(x = 10, y = 20)
+        self.__resultado1 = Label(self.__marco, textvariable = self.__r1).place(x = 80, y= 20)
+        self.l2 = Label(self.__marco, text="Area = ").place(x=10, y=70)
+        self.__resultado2 = Label(self.__marco, textvariable=self.__r2).place(x=80, y=70)
+        # Cuadrado
+        self.__r3 = StringVar()
+        self.__r4 = StringVar()
+        self.l3 = Label(self.__marco, text="Perimetro = ").place(x=210, y=20)
+        self.__resultado3 = Label(self.__marco, textvariable=self.__r3).place(x=280, y=20)
+        self.l4 = Label(self.__marco, text="Area = ").place(x=210, y=70)
+        self.__resultado4 = Label(self.__marco, textvariable=self.__r4).place(x=280, y=70)
+        # Triangulo
+        self.__r5 = StringVar()
+        self.__r6 = StringVar()
+        self.l5 = Label(self.__marco, text="Perimetro = ").place(x=410, y=20)
+        self.__resultado1 = Label(self.__marco, textvariable=self.__r5).place(x=480, y=20)
+        self.l6 = Label(self.__marco, text="Area = ").place(x=410, y=70)
+        self.__resultado2 = Label(self.__marco, textvariable=self.__r6).place(x=480, y=70)
         self.__marco.mainloop()
 
 
@@ -66,6 +94,7 @@ class circulo(Figuras):
         p = 2*math.pi*r
         return p
 
+
     def area(self):
         a = math.pi * (self.modulo()**2)
         return a
@@ -77,12 +106,15 @@ class cuadrado(Figuras):
     def __init__(self):
         Figuras.__init__(self)
 
+
     def perimetro(self):
-        p = (int(self.x)-int(self.antx))*4
+        self.__lx = math.sqrt((int(self.x) - int(self.antx)) ** 2)
+        self.__ly = math.sqrt((int(self.anty) - int(self.y)) ** 2)
+        p = (self.__lx*2) + (self.__ly*2)
         return p
 
     def area(self):
-        a = math.pi * (self.modulo()**2)
+        a = self.__lx * self.__ly
         return a
 
 class triangulo(Figuras):
@@ -91,30 +123,21 @@ class triangulo(Figuras):
         Figuras.__init__(self)
 
     def perimetro(self):
-        r = self.modulo
-        p = r + (int(self.x)-int(self.antx))+(int(self.anty)-int(self.y))
+        r = self.modulo()
+        self.__lx = math.sqrt((int(self.x) - int(self.antx)) ** 2)
+        self.__ly = math.sqrt((int(self.anty) - int(self.y)) ** 2)
+        p = r + self.__lx + self.__ly
         return p
 
     def area(self):
-        a = math.pi * (self.modulo()**2)
+        a = (self.__lx * self.__ly)/2
         return a
 
-    def dibujar(self,marco, canvas):
-
-        canvas.pack(expand=YES, fill=BOTH)
-        canvas.create_oval(10, 10, self.__modulo()*100, self.__modulo()*100, width=5, fill="yellow")
 
 
-# x1,y1,x2,y2 = int(input("Valor: ")),int(input("Valor: ")),int(input("Valor: ")),int(input("Valor: "))
-# obj = circulo(x1,y1,x2,y2)
-# obj2 = cuadrado(x1,y1,x2,y2)
-# print(str(obj.area()))
-# print(str(obj.perimetro()))
-# obj.dibujar(marco,canvas)
-# obj2.dibujar(marco,canvas)
-# marco.mainloop()
 
-obj = cuadrado()
 
-obj.mostrar()
-obj.perimetro()
+cuadrado = cuadrado()
+cuadrado.configurar()
+
+
